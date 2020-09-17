b64fad838d67a0e7b126ccc5f2db1fec9934c777
DanyZurita <dzurita@cifpfbmoll.eu>
2020-09-17 06:37:46 +0200

    feat(RomanValues): Añadir clase enum con valores de los simbolos romanos

---
+++ src/main/java/edu/elsmancs/romans/RomanValues.java
@@ -0,0 +1,19 @@
+package edu.elsmancs.romans;
+
+
+public enum RomanValues {
+		
+		I(1), V(5), X(10), L(50), C(100), D(500), M(1000),
+		IV(4), IX(9), XL(40), XC(90), CD(400), CM(900);
+		
+		private int decimalValue;
+		
+		private RomanValues(int valorDecimal){
+			this.decimalValue = valorDecimal;
+		}
+		
+		public int getDecimalValue(){
+			return this.decimalValue;
+		}
+
+}
